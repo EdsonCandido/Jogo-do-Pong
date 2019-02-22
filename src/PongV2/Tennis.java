40cfa47edf69203ee3e4af30822d3aea4e9a6f70
EdsonCandido <edson.c.a.j@hotmail.com>
2019-02-22 00:14:14 -0300

    Criado a Bola junto com a Função de Colisão

--- src/PongV2/Tennis.java
+++ src/PongV2/Tennis.java
@@ -13,10 +13,14 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 	Thread thread;
 	//Criando Jogadores
 	HumanPaddle p1;
+	//Criando a Bola
+	Ball b1;
+	
 	public void init() {
 		this.resize(WIDTH, HEIGHT);
 		this.addKeyListener(this);
 		p1 = new HumanPaddle(1);
+		b1 = new Ball();
 		thread = new Thread(this);
 		thread.start();
 
@@ -25,7 +29,14 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		p1.draw(g);
+		if(b1.getX() < -10 || b1.getX() > 710){
+			g.setColor(Color.red);
+			g.drawString("Perdeu ", 350, 250);
+		}else{
+			p1.draw(g);
+			b1.draw(g);	
+		}
+		
 	}
 
 	public void update(Graphics g) {
@@ -36,6 +47,8 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 		// TODO Auto-generated method stub
 		for (;;) {
 			p1.move();
+			b1.move();
+			b1.checkPaddleCollision(p1, p1);
 			repaint();
 			try {
 				Thread.sleep(10);
